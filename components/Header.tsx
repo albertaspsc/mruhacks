2666ae8a3fa05f9bcace1ce4a71a75b7cfa9718b
Archie <110494298+archie122@users.noreply.github.com>
2024-05-02 09:04:45 -0600

    Added somethings

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,21 +2,37 @@ import Image from "next/image";
 
 export default function Header() {
 	return (
-		<header className="w-full flex flex-row justify-between items-center bg-blue-900 text-white p-4 m-2">
-			<div className="flex flex-row items-center justify-center space-x-8">
-				<Image src="/mru_title.jpg" alt="MRUHacks" width={200} height={200} />
-				{/* <h1 className="text-xl font-bold text-white">MRUHacks</h1> */}
+		<div className="navbar bg-base-100">
+		<div className="navbar-start">
+			<div className="dropdown">
+			<div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 			</div>
-			<div className="flex flex-row-reverse p-4 m-4">
-				<span className="mx-4 px-2 text-white text-lg"><a>FAQ</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Sponsors</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>About</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Team</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Events</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Home</a></span>
+			<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+				<li><a>Home</a></li>
+				<li><a>FAQ</a></li>
+				<li><a>Sponsors</a></li>
+				<li><a>About</a></li>
+				<li><a>Team</a></li>
+				<li><a>Events</a></li>
+			</ul>
 			</div>
-
-		</header>
+			<a className="btn btn-ghost"><Image src="/mru_title.jpg" alt="MRUHacks" width={150} height={150} /></a>
+		</div>
+		<div className="navbar-center hidden lg:flex">
+			<ul className="menu menu-horizontal px-1">
+				<li><a>Home</a></li>
+				<li><a>FAQ</a></li>
+				<li><a>Sponsors</a></li>
+				<li><a>About</a></li>
+				<li><a>Team</a></li>
+				<li><a>Events</a></li>
+			</ul>
+		</div>
+		<div className="navbar-end">
+			<a className="btn">Button</a>
+		</div>
+		</div>
 	);
 }
 
@@ -63,3 +79,18 @@ export default function Header() {
 //         </header>
 //     );
 // }
+
+{/* <header className="w-full flex flex-row justify-between items-center bg-blue-900 text-white p-4 m-2">
+			<div className="flex flex-row items-center justify-center space-x-8">
+				<Image src="/mru_title.jpg" alt="MRUHacks" width={200} height={200} />
+			</div>
+			<div className="flex flex-row-reverse p-4 m-4">
+				<span className="mx-4 px-2 text-white text-lg"><a>FAQ</a></span>
+				<span className="mx-4 px-2 text-white text-lg"><a>Sponsors</a></span>
+				<span className="mx-4 px-2 text-white text-lg"><a>About</a></span>
+				<span className="mx-4 px-2 text-white text-lg"><a>Team</a></span>
+				<span className="mx-4 px-2 text-white text-lg"><a>Events</a></span>
+				<span className="mx-4 px-2 text-white text-lg"><a>Home</a></span>
+			</div>
+
+		</header> */}
