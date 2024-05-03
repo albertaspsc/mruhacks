dddd1ca96aa4daf9ac0fe2251cf378b3f99a603d
Amy Caufield <acauf564@mtroyal.ca>
2024-05-03 14:26:16 -0600

    Made question components collapsible

--- components/Question.tsx
+++ components/Question.tsx
@@ -3,10 +3,14 @@ export default function Question({ question, answer }: {
 	answer: string;
 }) {
 	return (
-		<div className="flex-1 p-4 bg-gray-200 my-2 rounded-2xl">
-			<h2 className="text-l font-semibold">{question}</h2>
-			<hr className="my-1" />
-			<p className="flex flex-row space-x-1 p-2 font-normal">{answer}</p>
+		<div className="collapse collapse-arrow p-4 m-4 border border-base-300 bg-base-200">
+			<input type="checkbox" />
+			<div className="collapse-title text-xl font-medium">
+				{question}
+			</div>
+			<div className="collapse-content">
+				<p>{answer}</p>
+			</div>
 		</div>
 	);
 }
