3e0301f0c6b046c6b920e4a2eba88bd63ffe7f2f
Archie <110494298+archie122@users.noreply.github.com>
2024-05-03 19:01:21 -0600

    Added a basic Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import FAQs from "@/components/FAQ";
 import Welcome from "@/components/Welcome";
+import AboutUs from "@/components/AboutUs";
+import AboutHacks from "@/components/AboutHacks";
+
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center">
       <Welcome />
-      <FAQs />
+      <AboutUs />
+      <AboutHacks />
+
 
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 
+
 export default function Header() {
 	return (
 		<div className="navbar bg-base-100">
@@ -35,62 +36,3 @@ export default function Header() {
 		</div>
 	);
 }
-
-// 'use client'
-
-// import Image from 'next/image';
-// import React from 'react';
-
-// export default function Header() {
-//     const [hovered, setHovered] = React.useState(false);
-
-//     return (
-//         <header className="w-full flex flex-row justify-between items-center bg-blue-700 text-white p-4 m-2">
-//             <div className="flex flex-row items-center justify-center space-x-8">
-//                 <div
-//                     onMouseEnter={() => setHovered(true)}
-//                     onMouseLeave={() => setHovered(false)}
-//                 >
-//                     {hovered ? (
-//                         <Image
-//                             src="/mru_title.jpg"
-//                             alt="MRUHacks"
-//                             width={128}
-//                             height={128}
-//                         />
-//                     ) : (
-//                         <Image
-//                             src="/mru_hacks.jpg"
-//                             alt="MRUHacks"
-//                             width={64}
-//                             height={64}
-//                         />
-//                     )}
-//                 </div>
-//             </div>
-//             <div className="flex flex-row-reverse p-4 m-4">
-//                 <span className="mx-4 px-2 text-white text-lg"><a>FAQ</a></span>
-//                 <span className="mx-4 px-2 text-white text-lg"><a>Sponsors</a></span>
-//                 <span className="mx-4 px-2 text-white text-lg"><a>About</a></span>
-//                 <span className="mx-4 px-2 text-white text-lg"><a>Team</a></span>
-//                 <span className="mx-4 px-2 text-white text-lg"><a>Events</a></span>
-//                 <span className="mx-4 px-2 text-white text-lg"><a>Home</a></span>
-//             </div>
-//         </header>
-//     );
-// }
-
-{/* <header className="w-full flex flex-row justify-between items-center bg-blue-900 text-white p-4 m-2">
-			<div className="flex flex-row items-center justify-center space-x-8">
-				<Image src="/mru_title.jpg" alt="MRUHacks" width={200} height={200} />
-			</div>
-			<div className="flex flex-row-reverse p-4 m-4">
-				<span className="mx-4 px-2 text-white text-lg"><a>FAQ</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Sponsors</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>About</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Team</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Events</a></span>
-				<span className="mx-4 px-2 text-white text-lg"><a>Home</a></span>
-			</div>
-
-		</header> */}
