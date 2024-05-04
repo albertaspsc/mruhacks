241cb0848c9777a0cf430a8c703fd4e914e0bca4
Amy Caufield <acauf564@mtroyal.ca>
2024-05-04 17:03:41 -0600

    Fixed bad stash resolve

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 import FAQs from "@/components/FAQ";
 import Welcome from "@/components/Welcome";
 import Community from "@/components/Community";
-<<<<<<< Updated upstream
-=======
 import AboutUs from "@/components/AboutUs";
 import AboutHacks from "@/components/AboutHacks";
 import Sponsors from "@/components/Sponsors";
 
->>>>>>> Stashed changes
 export default function Home() {
   return (
     <div className="w-full flex flex-col items-center">
       <Welcome />
+      <AboutUs />
+      <AboutHacks />
       <FAQs />
       <Community />
-<<<<<<< Updated upstream
-
-=======
       <Sponsors />
->>>>>>> Stashed changes
     </div>
   );
 }
