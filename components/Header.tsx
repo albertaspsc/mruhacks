821cf3980b1c24fb3242b82330591a0538426ba5
Archie <110494298+archie122@users.noreply.github.com>
2024-05-06 17:58:52 -0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,46 +1,6 @@
 'use client'
 
 import Image from "next/image";
-
-
-// export default function Header() {
-// 	return (
-// 		<div className="navbar sticky top-1 bg-base-900">
-// 			<div className="navbar-start flex-row flex-initial">
-// 				<div className="dropdown">
-// 					<div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-// 						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-// 					</div>
-// 					<ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-// 						<li><a href="#">Home</a></li>
-// 						<li><a href="#">About MRUHacks</a></li>
-// 						<li><a href="#">FAQs</a></li>
-// 						<li><a href="#">Our Community</a></li>
-// 						<li><a href="#">Sponsors</a></li>
-// 					</ul>
-// 				</div>
-// 				<a className="btn btn-ghost">
-// 					<Image className="dark:hidden"src="/mru_title_dark.jpg" alt="MRUHacks" width={150} height={150} />
-// 					<Image className="hidden dark:block" src="/mru_title_light.jpg" alt="MRUHacks" width={150} height={150} />
-// 				</a>
-// 			</div>
-
-// 			<div className="navbar-end hidden md:flex flex-row items-end">
-// 				<div className="flex-none">
-// 					<ul className="menu menu-md menu-horizontal px-1">
-// 						<li><a href="#">Home</a></li>
-// 						<li><a href="#">About MRUHacks</a></li>
-// 						<li><a href="#">FAQs</a></li>
-// 						<li><a href="#">Our Community</a></li>
-// 						<li><a href="#">Sponsors</a></li>
-// 					</ul>
-// 				</div>
-// 			</div>
-// 		</div >
-// 	);
-// }
-
-
 import { useState, useEffect } from 'react';
 
 export default function Header () {
