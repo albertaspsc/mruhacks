b7b1315504763e460bf061c5a20e49d10a32e36d
Amy Caufield <acauf564@mtroyal.ca>
2024-05-23 15:52:16 -0600

    Inserted placeholder image for collage on right

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Welcome() {
   return (
     <div
@@ -8,8 +10,9 @@ export default function Welcome() {
       <div className="hero-overlay">
         {/* <img src="/bg.jpg" alt="" /> */}
       </div>
-      <div className="hero-content text-center text-primary-500 p-10 opacity-90">
-        <div className="max-w-md">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Image className="mt-8" src="/bg.jpg" alt="" width={1200} height={600} />
+        <div className="max-w-md text-center text-primary-500 p-10 opacity-90 lg:text-left">
           <h1 className="mb-5 text-7xl font-bold p-10">MRUHacks</h1>
           <p className="mb-5 text-xl font-semibold p-8">
             24 Hours of Collaboration, Coding, and Connections
