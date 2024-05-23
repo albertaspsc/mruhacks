758856c616093bada1d9072b141a6585f2600db5
Amy Caufield <acauf564@mtroyal.ca>
2024-05-23 14:49:58 -0600

    Commented out image for hero section
    Brightened text for MRUHacks and subtitle

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -3,18 +3,18 @@ export default function Welcome() {
     <div
       className="hero min-h-screen bg-opacity-500"
       id="home"
-      style={{ backgroundImage: "url(bg.jpg)"}}
+    // style={{ backgroundImage: "url(bg.jpg)" }}
     >
       <div className="hero-overlay">
         {/* <img src="/bg.jpg" alt="" /> */}
       </div>
-      <div className="hero-content text-center text-neutral-content p-10 opacity-90 bg-primary-700 rounded-2xl">
+      <div className="hero-content text-center text-primary-500 p-10 opacity-90">
         <div className="max-w-md">
-          <h1 className="mb-5 text-7xl font-bold text-text-200">MRUHacks</h1>
-          <p className="mb-5 text-text-200 text-lg">
+          <h1 className="mb-5 text-7xl font-bold p-10">MRUHacks</h1>
+          <p className="mb-5 text-xl font-semibold p-8">
             24 Hours of Collaboration, Coding, and Connections
           </p>
-          <button className="btn bg-accent-700 hover:bg-accent-500 border-0 text-white">Join Us</button>
+          <button className="btn bg-gray-300 border-0 text-black disabled">Join Us</button>
         </div>
       </div>
     </div>
