29269eaddf066b11f9d69156b6109df5aabfb1d9
Amy Caufield <acauf564@mtroyal.ca>
2024-05-24 15:57:23 -0600

    Minor fixes for Matt
    - Changed MRUHacks colour for visibility on image
    - Added link for pre-reg
    - Included names for social media platforms

--- components/Community.tsx
+++ components/Community.tsx
@@ -15,39 +15,48 @@ export default function Community() {
       <div className="mb-8">
         <Image
           src="/group_image_vlad4.jpg"
-          alt="MRUHacks 2023 participants"
+          alt="MRUHacks 2023"
           className="rounded"
           width={1600}
           height={0}
         />
       </div>
-      <div className="flex flex-wrap mb-4 text-primary-500">
+      <div className="flex flex-wrap mb-4 space-x-8 text-primary-500">
         {" "}
         {/* Added flex-wrap for responsive wrapping */}
-        <a
-          href="https://www.instagram.com/mruhacks"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-active btn-link mr-4 mb-2"
-        >
-          <FaInstagram size={24} />
-        </a>
-        <a
-          href="https://www.example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-active btn-link mr-4 mb-2"
-        >
-          <FaDiscord size={24} />
-        </a>
-        <a
-          href="https://www.linkedin.com/company/mruhacks"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btn btn-active btn-link mb-2"
-        >
-          <FaLinkedinIn size={24} />
-        </a>
+        <div className="flex flex-col content-center mb-2">
+          <a
+            href="https://www.instagram.com/mruhacks"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-active btn-link"
+          >
+            <FaInstagram size={24} />
+            <p className="text-md font-medium">Instagram</p>
+          </a>
+        </div>
+        <div className="flex flex-col content-center mb-2">
+          <a
+            href="https://www.example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-active btn-link"
+          >
+            <FaDiscord size={24} />
+            <p className="text-md font-medium">Discord</p>
+          </a>
+        </div>
+        <div className="flex flex-col justify-center mb-2">
+          <a
+            href="https://www.linkedin.com/company/mruhacks"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-active btn-link"
+          >
+            <FaLinkedinIn size={24} />
+            <p className="text-md font-medium">LinkedIn</p>
+          </a>
+        </div>
       </div>
     </div>
   );
--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -4,13 +4,17 @@ export default function Welcome() {
       className="hero min-h-screen bg-opacity-500"
       id="home" style={{ backgroundImage: "url(bg3.jpg)" }}
     >
-      <div className="hero-content text-center text-primary-600 p-10 opacity-90">
+      <div className="hero-content text-center text-white p-10 opacity-90">
         <div className="max-w-full p-10">
           <h1 className="mb-5 text-7xl font-extrabold p-4">MRUHacks</h1>
-          <p className="mb-5 text-xl text-white font-semibold p-4">
+          <p className="mb-5 text-xl font-semibold p-4">
             24 Hours of Collaboration, Coding, and Connections
           </p>
-          <button className="btn disabled bg-gray-300 btn-active:bg-secondary-500 border-0 btn-lg text-black">Register for MRUHacks 2024!</button>
+          <a href="https://forms.gle/3E7CXnPtpGGGuN3y7" target="_blank">
+            <button className="btn bg-secondary-500 text-xl border-0 btn-xl text-white font-semibold hover:bg-secondary-300 hover:text-black">
+              Pre-register now!
+            </button>
+          </a>
         </div>
       </div>
     </div>
