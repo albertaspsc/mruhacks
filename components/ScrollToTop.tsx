c19daf2ea7f4188c82395e6fba91a9f9cd420778
Amy Caufield <acauf564@mtroyal.ca>
2024-05-25 21:35:26 -0600

    Hid to-top on mobile/tablet

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -38,7 +38,7 @@ export const ScrollToTop = () => {
         onClick={scrollToTop}
         className={classNames(
           isVisible ? "opacity-100" : "opacity-0",
-          "bg-primary-600 hover:bg-primary-700 focus:ring-primary inline-flex items-center rounded-full p-3 text-primary shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2",
+          "hidden lg:inline-flex lg:bg-primary-600 hover:bg-primary-700 focus:ring-primary items-center rounded-full p-3 text-primary shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2",
         )}
       >
         <BiArrowFromBottom className="h-6 w-6" aria-hidden="true" />
