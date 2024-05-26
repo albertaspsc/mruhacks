19b8357fa5682d6f158e557aaa1749822a366413
Amy Caufield <acauf564@mtroyal.ca>
2024-05-25 18:50:32 -0600

    Removed text animation on FAQ open/close

--- components/Question.tsx
+++ components/Question.tsx
@@ -6,14 +6,14 @@ export default function Question({
   answer: string;
 }) {
   return (
-    <div className="collapse collapse-arrow join-item p-4 my-4 border border-accent-100 bg-accent-700">
-      <input type="checkbox" name="faq" />
-      <div className="collapse-title text-lg lg:text-xl font-semibold text-secondary-200">
+    <details className="collapse collapse-arrow join-item p-4 my-4 border border-accent-100 bg-accent-700">
+      <input type="checkbox" name="faq" className="hidden" />
+      <summary className="collapse-title text-lg lg:text-xl font-semibold text-secondary-200">
         {question}
-      </div>
+      </summary>
       <div className="collapse-content text-md text-text-50 font-medium lg:text-lg">
         <p>{answer}</p>
       </div>
-    </div>
+    </details>
   );
 }
