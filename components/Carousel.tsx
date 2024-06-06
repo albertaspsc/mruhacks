0e6e8eace7f6a7561c7b240a63d72c86f7214d3b
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-06-05 20:50:49 -0600

    Add Image Carousel and Shadcn Setupfor it

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,127 +1,37 @@
 import Image from "next/image";
+import carousel_items from "@/assets/carousel_images";
+import { ArrayElement } from "@/components/utils";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+type CarouselImageTakes = ArrayElement<typeof carousel_items>;
+const CarouselImage = ({ ...props }: CarouselImageTakes) => {
+  return (
+    <CarouselItem className="flex justify-center items-center flex-col basis-[50%]">
+      {/*This is actually passed eslint is just stupid here*/}
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image {...props} className=" rounded-2xl " />
+      <p>{props.legend}</p>
+    </CarouselItem>
+  );
+};
 
 export default function Home() {
   return (
-    <>
-      <div className="w-full flex flex-col items-center bg-background-100">
-        <div className="carousel w-5/6">
-          <div id="slide1" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (1).jpg"
-              alt="Slide 2"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide7" className="btn btn-circle"></a>
-              <a href="#slide2" className="btn btn-circle"></a>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (2).jpg"
-              alt="Slide 3"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (9).jpg"
-              alt="Slide 4"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide4" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (4).jpg"
-              alt="Slide 5"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide5" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide5" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (5).jpg"
-              alt="Slide 6"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide6" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide6" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (6).jpg"
-              alt="Slide 7"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide5" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide7" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide7" className="carousel-item relative w-full">
-            <Image
-              src="/last_year (7).jpg"
-              alt="Slide 8"
-              className="rounded"
-              width={6000}
-              height={4000}
-            />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide6" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    <Carousel opts={{ loop: true }} className="">
+      <CarouselContent>
+        {carousel_items.map((item, key) => (
+          <CarouselImage {...item} key={key} />
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="left-0 bg-secondary " />
+      <CarouselNext className="right-0 bg-secondary" />
+    </Carousel>
   );
 }
