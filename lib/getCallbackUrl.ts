3594de870e229a8ae60a829c205575210a1a3713
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-06-20 21:34:48 -0600

    I guess we use headers now

--- lib/getCallbackUrl.ts
+++ lib/getCallbackUrl.ts
@@ -1,12 +1,7 @@
 import { headers } from "next/headers";
 
 export const getCallbackUrl = () => {
-  let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ??
-    process?.env?.VERCEL_BRANCH_URL ??
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-    headers().get("origin") ??
-    "http://localhost:3000/";
+  let url = headers().get("origin") ?? "http://localhost:3000/";
 
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
