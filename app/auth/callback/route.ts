f6b4600c7d0ccc599b31870da8f657d4b0bf5c56
Amy Caufield <acauf564@mtroyal.ca>
2024-06-21 14:38:16 -0600

    Added redirect to /apply on sign in

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get("next") ?? "/";
+  const next = searchParams.get("next") ?? "";
 
   if (code) {
     const cookieStore = cookies();
@@ -30,7 +30,7 @@ export async function GET(request: Request) {
 
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
+      return NextResponse.redirect(`${origin}/${next}`);
     }
   }
 
