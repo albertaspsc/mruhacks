70bae67d6492ae77ac6c8c2cda2be9fb937d4b3d
Amy Caufield <acauf564@mtroyal.ca>
2024-06-30 19:25:51 -0600

    Added message template for reg confirmation

--- app/mailer/index.ts
+++ app/mailer/index.ts
@@ -6,93 +6,59 @@ import Handlebars from "handlebars";
 import { convert } from "html-to-text";
 import inquirer from "inquirer";
 import papa from "papaparse";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-const transporter = nodemailer.createTransport({
+const options: SMTPTransport.Options = {
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  // secure: true,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-  pool: false,
-  maxConnections: 1,
-  rateLimit: 10,
-});
+};
 
-const acceptance = Handlebars.compile(
-  (await readFile("./acceptance.hbs")).toString(),
-);
-const rejection = Handlebars.compile(
-  (await readFile("./rejection.hbs")).toString(),
-);
-const global_styles = (await readFile("./styles.css")).toString();
-const email_signature = (await readFile("./email_signature.html")).toString();
+const transporter = nodemailer.createTransport(options);
+
+const confirmation = Handlebars.compile(readFile("./regConf.hbs").toString());
+const prereg = Handlebars.compile(readFile("./preregBlast.hbs").toString());
 
 const normalizeHeader = (header: string) =>
   header.toLowerCase().trim().replace(/\s+/g, "_");
 
-const data: [any] = papa.parse((await readFile("./data.csv")).toString(), {
+{
+  /* const data: [any] = papa.parse((await readFile("./data.csv")).toString(), {
   header: true,
   dynamicTyping: true,
   skipEmptyLines: true,
   transformHeader: normalizeHeader,
 }).data as [any];
-
-async function send_all() {
-  await Promise.all(
-    data.map(async (element: any) => {
-      try {
-        const all_vars = {
-          ...element,
-          email_signature,
-          global_styles,
-          subject:
-            element.hired == "Yes"
-              ? "Welcome to the MRUHacks Organizing Team!"
-              : "MRUHacks Organizing Team",
-        };
-
-        const html =
-          element.hired == "Yes"
-            ? acceptance({
-                ...all_vars,
-              })
-            : rejection({
-                ...all_vars,
-              });
-
-        const response = await transporter.sendMail({
-          from: process.env.FROM_TEXT,
-          to: all_vars.email,
-          subject: all_vars.subject,
-          text: convert(html),
-          html: html,
-        });
-        console.log(response);
-        return response;
-      } catch (e) {
-        console.log(e);
-        console.log(`Failed to send email to ${JSON.stringify(element)}`);
-      }
-    }),
-  );
+*/
 }
 
-if (process.env.SMTP_HOST !== "localhost" || 1) {
-  const email_count = data.length;
+export default async function send_confirmation(user: string) {
+  try {
+    const message = {
+      from: "hello@mruhacks.ca",
+      to: user,
+      subject: "Your MRUHacks 2024 Application",
+    };
 
-  const confirm = await inquirer.prompt([
-    {
-      name: "confirm",
-      type: "confirm",
-      message: `About to send ${email_count} emails, using '${process.env.SMTP_HOST}'`,
-    },
-  ]);
+    const html = confirmation({ ...message });
 
-  if (confirm.confirm) {
-    await send_all();
+    const response = await transporter.sendMail({
+      from: process.env.FROM_TEXT,
+      to: user,
+      subject: message.subject,
+      text: convert(html),
+      html: html,
+    });
+    console.log(response);
+    return response;
+  } catch (e) {
+    console.log(e);
+    console.log(`Failed to send email to ${JSON.stringify(user)}`);
   }
-
-  transporter.close();
 }
+
+transporter.close();
