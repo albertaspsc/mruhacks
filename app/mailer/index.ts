f113e1c37743490e5e5357a5aca90900c0f5645d
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-07-02 08:54:45 -0600

    try again

--- app/mailer/index.ts
+++ app/mailer/index.ts
@@ -6,7 +6,7 @@ import SMTPTransport from "nodemailer/lib/smtp-transport";
 const options: SMTPTransport.Options = {
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
