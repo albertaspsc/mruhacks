82cdbdd9f961e633bc63a9a4706be909fc7c579f
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-07-04 10:24:40 -0600

    convert to sponsor us button to non daisy ui version

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
 export default function Sponsors() {
   return (
     <div id="sponsors" className="space-y-5">
@@ -8,12 +11,11 @@ export default function Sponsors() {
         <p className="font-medium text-center mb-4">
           MRUHacks would be impossible without our fantastic sponsors.
         </p>
-        <a
-          href="mailto:outreach@mruhacks.ca"
-          className="text-neutral btn btn-primary btn-lg"
-        >
-          Become a Sponsor!
-        </a>
+        <Button className="h-16 min-h-16 px-5 text-lg text-base-100" asChild>
+          <Link href="mailto:outreach@mruhacks.ca" className="">
+            Become a Sponsor!
+          </Link>
+        </Button>
       </div>
     </div>
   );
