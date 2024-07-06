4b5a19e3cd6120830224bd44edd8de343bfcd75b
Thomas Kapocsi <tkapo261@mtroyal.ca>
2024-07-05 19:06:50 -0600

    Fix image scalling on 403 page

--- components/Unauthorized.tsx
+++ components/Unauthorized.tsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center max-w-screen-md m-auto">
-      <Image src={blue_screen} alt="crt montor with windows blue screen " />
+    <div className="flex flex-col items-center justify-center">
+      <Image
+        src={blue_screen}
+        className="h-[30vh] w-auto"
+        alt="crt montor with windows blue screen "
+      />
       <h1 className="text-xl text-base-content">403 Unauthorized Request</h1>
       <p>How dare you!</p>
       <Link href="/" className="underline">
