8b06c4b2f2c736e678b63ea7cac5beeb781c8d43
Amy Caufield <acauf564@mtroyal.ca>
2024-07-07 16:37:21 -0600

    Progress on admin charts

--- app/admin/applications/stats/charts.tsx
+++ app/admin/applications/stats/charts.tsx
@@ -5,18 +5,21 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
-  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
+  PieChart,
+  Pie,
 } from "recharts";
+
 export function GenderDemoGraphics({
   gender_demographics,
 }: {
   gender_demographics: Record<string, unknown>[];
 }) {
   return (
-    <ResponsiveContainer>
+    <>
+      <h1 className="text-center">User Demographics</h1>
       <BarChart width={400} height={400} data={gender_demographics!}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="gender" />
@@ -25,6 +28,46 @@ export function GenderDemoGraphics({
         <Legend />
         <Bar dataKey="count" fill="#8884d8" />
       </BarChart>
-    </ResponsiveContainer>
+    </>
+  );
+}
+
+export function DietaryRequirements({
+  data,
+}: {
+  data: Record<string, unknown>[];
+}) {
+  return (
+    <>
+      <h1 className="text-center">Dietary Needs</h1>
+      <BarChart width={400} height={400} data={data!}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="dietary" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="count" fill="#8884d8" />
+      </BarChart>
+    </>
+  );
+}
+
+export function PieChartGraph({ data }: { data: Record<string, unknown>[] }) {
+  return (
+    <>
+      <PieChart width={400} height={400}>
+        <Pie
+          dataKey="count"
+          isAnimationActive={false}
+          data={data!}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#8884d8"
+          label
+        />
+        <Tooltip />
+      </PieChart>
+    </>
   );
 }
--- app/admin/applications/stats/page.tsx
+++ app/admin/applications/stats/page.tsx
@@ -1,7 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ReactNode, Suspense } from "react";
 import { createClient } from "@/lib/supabase/server";
-import { GenderDemoGraphics } from "./charts";
+import {
+  DietaryRequirements,
+  GenderDemoGraphics,
+  PieChartGraph,
+} from "./charts";
 
 function StatsCard({
   title,
@@ -34,7 +38,6 @@ export default async function Page() {
       if (error) console.error(error);
       return data?.[0]?.count ?? undefined;
     });
-
   const user_count = await supabase
     .from("users")
     .select("count")
@@ -42,7 +45,6 @@ export default async function Page() {
       if (error) console.error(error);
       return data?.[0]?.count ?? undefined;
     });
-
   const gender_demographics = await supabase
     .from("gender_demographics")
     .select()
@@ -50,12 +52,70 @@ export default async function Page() {
       if (error) console.error(error);
       return data;
     });
+  const race_demographics = await supabase
+    .from("race_demographics")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
+  const ethnicity_count = await supabase
+    .from("ethnicity_count")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
+  const major_count = await supabase
+    .from("major_count")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
+  const university_count = await supabase
+    .from("university_count")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
+
+  const study_year = await supabase
+    .from("year_count")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
+
+  const dietary_needs = await supabase
+    .from("dietary_restrictions")
+    .select()
+    .then(({ data, error }) => {
+      if (error) console.error(error);
+      return data;
+    });
 
   return (
-    <div className="grid grid-cols-2 gap-5">
-      <StatsCard title="Total Users" value={user_count ?? "<Unknown>"} />
-      <StatsCard title="Total Registrations" value={reg_count ?? "<Unknown>"} />
-      <GenderDemoGraphics gender_demographics={gender_demographics!} />
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-5">
+        <StatsCard title="Total Users" value={user_count ?? "<Unknown>"} />
+        <StatsCard
+          title="Total Registrations"
+          value={reg_count ?? "<Unknown>"}
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <DietaryRequirements data={dietary_needs!} />
+        <GenderDemoGraphics gender_demographics={gender_demographics!} />
+        <PieChartGraph data={race_demographics!} />
+
+        <PieChartGraph data={university_count!} />
+        <PieChartGraph data={ethnicity_count!} />
+        <PieChartGraph data={major_count!} />
+        <PieChartGraph data={study_year!} />
+      </div>
+    </>
   );
 }
