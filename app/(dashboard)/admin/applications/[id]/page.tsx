8308262f879735058b1168d575663152d3e0151b
matthew-hre <whycardboard@gmail.com>
2024-07-14 14:31:38 -0600

    updated admin application preview pages

--- app/(dashboard)/admin/applications/[id]/page.tsx
+++ app/(dashboard)/admin/applications/[id]/page.tsx
@@ -29,111 +29,115 @@ async function Application({ id }: { id: string }) {
   if (!application) return "Unknown Application";
 
   return (
-    <Card className="m-5">
-      <CardContent>
-        <CardHeader className="flex flex-row items-start bg-muted/50 pt-5">
-          <div className="grid gap-0.5">
-            <CardTitle className="group flex items-center gap-2 text-lg">
-              Application: {application.name}
-            </CardTitle>
-            <CardDescription>
-              Status: {application.application_status} <br />
-              Date: {new Date(application.timeSubmitted).toLocaleDateString()}
-            </CardDescription>
-          </div>
-          <div className="ml-auto flex items-center gap-1">
-            <Button size="sm" variant="outline" className="h-8 gap-1">
-              <Check className="h-3.5 w-3.5" />
-              <span className="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
-                Approve Application
-              </span>
-            </Button>
-          </div>
-        </CardHeader>
-        <div className="grid gap-3">
-          <div className="font-semibold">Applicant Details</div>
-          <ul className="grid gap-3">
-            <li className="flex items-center justify-between">
-              <span className="text-muted-foreground">University</span>
-              <span>{application.university}</span>
-            </li>
-            <li className="flex items-center justify-between">
-              <span className="text-muted-foreground">Major</span>
-              <span>{application.major}</span>
-            </li>
-            <li className="flex items-center justify-between">
-              <span className="text-muted-foreground">Year</span>
-              <span>{application.year}</span>
-            </li>
-            <li className="flex items-center justify-between">
-              <span className="text-muted-foreground">Hackathons Attended</span>
-              <span>{application.hackathons}</span>
-            </li>
-          </ul>
-          <Separator className="my-2" />
-          <ul className="grid gap-3"></ul>
-        </div>
-        <Separator className="my-4" />
-        <div className="grid gap-3">
-          <div className="font-semibold">Contact Information</div>
-          <address className="grid gap-0.5 not-italic text-muted-foreground">
-            <span>{application.name}</span>
-            <span>
-              <a href={`mailto:${application.email}`}>{application.email}</a>
+    <div>
+      <div className="flex flex-row items-start bg-muted/50 py-5">
+        <div className="grid gap-0.5">
+          <CardTitle className="group flex flex-row flex-wrap items-baseline text-xl">
+            <span className="mr-1">{application.name}</span>
+            <span className="text-muted-foreground text-lg">
+              (<a href={`mailto:${application.email}`}>{application.email}</a>)
             </span>
-          </address>
+          </CardTitle>
+          <CardDescription>
+            {application.application_status} -{" "}
+            {new Date(application.timeSubmitted).toLocaleDateString()}
+          </CardDescription>
         </div>
-
-        <Separator className="my-4" />
-        <div className="grid auto-rows-max gap-3">
-          <div className="font-semibold">Links</div>
-          <div className="text-muted-foreground">
-            <dl>
-              <div className="flex items-center justify-between">
-                <dt>Github</dt>
-                <dd>{application.github}</dd>
-              </div>
-            </dl>
-            <dl>
-              <div className="flex items-center justify-between">
-                <dt>linkedin</dt>
-                <dd>{application.linkedin}</dd>
-              </div>
-            </dl>
-            <dl>
-              <div className="flex items-center justify-between">
-                <dt>Personal Site</dt>
-                <dd>{application.personalSite}</dd>
-              </div>
-            </dl>
-          </div>
+      </div>
+      <div className="grid gap-3 mb-4">
+        <div className="font-semibold">Applicant Details</div>
+        <ul className="flex flex-row">
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">University</span>
+            <span>{application.university}</span>
+          </li>
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">Major</span>
+            <span>{application.major}</span>
+          </li>
+        </ul>
+        <ul className="flex flex-row">
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">Year</span>
+            <span>{application.year}</span>
+          </li>
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">
+              Hackathons Attended
+            </span>
+            <span>{application.hackathons}</span>
+          </li>
+        </ul>
+      </div>
+      <div className="grid auto-rows-max gap-3 mb-4">
+        <div className="font-semibold">Links</div>
+        <ul className="grid gap-3">
+          {application.github && (
+            <li className="flex flex-col">
+              <span className="text-muted-foreground text-sm">GitHub</span>
+              <a
+                href={
+                  application.github.startsWith("http")
+                    ? application.github
+                    : `https://github.com/${application.github}`
+                }
+              >
+                {application.github}
+              </a>
+            </li>
+          )}
+          {application.linkedin && (
+            <li className="flex flex-col">
+              <span className="text-muted-foreground text-sm">LinkedIn</span>
+              <a
+                href={
+                  application.linkedin.startsWith("http")
+                    ? application.linkedin
+                    : `https://linkedin.com/in/${application.linkedin}`
+                }
+              >
+                {application.linkedin}
+              </a>
+            </li>
+          )}
+          {application.portfolio && (
+            <li className="flex flex-col">
+              <span className="text-muted-foreground text-sm">
+                Personal Site
+              </span>
+              <a href={application.portfolio}>{application.portfolio}</a>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div className="grid gap-3 mb-4">
+        <div className="font-semibold">Additional Information</div>
+        <ul className="flex flex-row">
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">
+              Dietary Preferences
+            </span>
+            <span>{application.dietary || "N/A"}</span>
+          </li>
+          <li className="flex flex-col flex-1">
+            <span className="text-muted-foreground text-sm">
+              Sponsor Consent
+            </span>
+            <span>{application.sponsorConsent}</span>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-row items-center border-t bg-muted/50 px-2 pt-4 pb-0">
+        <div className="ml-auto flex items-center gap-1">
+          <Button size="sm" variant="outline" className="h-8 gap-1">
+            <Check className="h-3.5 w-3.5" />
+            <span className="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
+              Approve Application
+            </span>
+          </Button>
         </div>
-        <Separator className="my-4" />
-        <div className="grid gap-3">
-          <div className="font-semibold">Additional Information</div>
-          <dl className="grid gap-3">
-            <div className="flex items-center justify-between">
-              <dt className="text-muted-foreground">Dietary Preferences</dt>
-              <dd>{application.dietary || "N/A"}</dd>
-            </div>
-          </dl>
-          <dl>
-            <div className="flex items-center justify-between">
-              <dt>Sponsor Consent</dt>
-              <dd>{application.sponsorConsent}</dd>
-            </div>
-          </dl>
-        </div>
-        <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
-          <div className="text-xs text-muted-foreground">
-            Submitted{" "}
-            <time dateTime={application.timeSubmitted}>
-              {new Date(application.timeSubmitted).toLocaleDateString()}
-            </time>
-          </div>
-        </CardFooter>
-      </CardContent>
-    </Card>
+      </div>
+    </div>
   );
 }
 
