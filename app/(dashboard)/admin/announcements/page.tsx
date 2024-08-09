8b1d8516c362d1f0d3ff5cdd446ebe604bed269a
Amy Caufield <acauf564@mtroyal.ca>
2024-08-08 21:07:45 -0600

    Added Discord support for announcements

--- app/(dashboard)/admin/announcements/page.tsx
+++ app/(dashboard)/admin/announcements/page.tsx
@@ -46,6 +46,18 @@ export default function Page() {
         email: data.email,
       })
       .select();
+
+    if (data.discord) {
+      let messageContent = "# " + data.title + "\n\n" + data.content;
+      await fetch(process.env.DISCORD_HOOK, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          content: messageContent,
+          username: "AnnouncementBot",
+        }),
+      });
+    }
   };
 
   return (
